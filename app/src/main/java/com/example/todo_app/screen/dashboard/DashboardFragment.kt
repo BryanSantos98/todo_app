2e6fede2c7c8196db29fc1ef2f689a93693fb23a
L-Mosca <88050952+L-Mosca@users.noreply.github.com>
2023-01-30 22:42:18 -0300

    Add new fragments to view pager and set basic screens transitions.

--- app/src/main/java/com/example/todo_app/screen/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/todo_app/screen/dashboard/DashboardFragment.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.todo_app.databinding.FragmentDashboardBinding
+import com.example.todo_app.screen.dashboard.adapter.ViewPagerAdapter
+import com.example.todo_app.screen.dashboardagents.AgentsDashboardFragment
+import com.example.todo_app.screen.dashboardassets.AssetsDashboardFragment
+import com.example.todo_app.screen.dashboardhelpcenter.HelpCenterDashboardFragment
+import com.example.todo_app.screen.dashboardhome.HomeDashboardFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -16,6 +20,7 @@ class DashboardFragment : Fragment() {
     private val binding get() = _binding!!
 
     val viewModel: DashboardViewModel by viewModels()
+    lateinit var adapter: ViewPagerAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,17 +33,33 @@ class DashboardFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        adapter = ViewPagerAdapter(this)
         initViews()
         initObservers()
     }
 
     private fun initViews() {
-
+        setViewPager()
+        setBottomNavigationListener()
     }
 
     private fun initObservers() {
-
+        viewModel.showHomeDashboard.observe(viewLifecycleOwner) {
+            if (binding.vpDashboardScreen.currentItem < adapter.itemCount - 1)
+                binding.vpDashboardScreen.currentItem += 1
+        }
+        viewModel.showAssetsDashboard.observe(viewLifecycleOwner) {
+            if (binding.vpDashboardScreen.currentItem < adapter.itemCount - 1)
+                binding.vpDashboardScreen.currentItem += 1
+        }
+        viewModel.showHelpCenterDashboard.observe(viewLifecycleOwner) {
+            if (binding.vpDashboardScreen.currentItem < adapter.itemCount - 1)
+                binding.vpDashboardScreen.currentItem += 1
+        }
+        viewModel.showAgentsDashboard.observe(viewLifecycleOwner) {
+            if (binding.vpDashboardScreen.currentItem < adapter.itemCount - 1)
+                binding.vpDashboardScreen.currentItem += 1
+        }
     }
 
     override fun onDestroyView() {
@@ -46,5 +67,21 @@ class DashboardFragment : Fragment() {
         _binding = null
     }
 
+    private fun setViewPager() {
+        adapter.addFragment(HomeDashboardFragment())
+        adapter.addFragment(AssetsDashboardFragment())
+        adapter.addFragment(HelpCenterDashboardFragment())
+        adapter.addFragment(AgentsDashboardFragment())
 
+        binding.vpDashboardScreen.adapter = adapter
+
+        // set animation to navigate between view pager fragments
+        //viewPager.setPageTransformer(YourPageTransformer())
+    }
+
+    private fun setBottomNavigationListener() {
+        binding.bnvDashboard.setOnItemSelectedListener { menuItem ->
+            viewModel.selectBottomNavItem(menuItem)
+        }
+    }
 }
--- app/src/main/java/com/example/todo_app/screen/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/example/todo_app/screen/dashboard/DashboardViewModel.kt
@@ -1,9 +1,40 @@
 package com.example.todo_app.screen.dashboard
 
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.todo_app.R
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class DashboardViewModel @Inject constructor() : ViewModel() {
+
+    val showHomeDashboard = MutableLiveData<Unit>()
+    val showAssetsDashboard = MutableLiveData<Unit>()
+    val showHelpCenterDashboard = MutableLiveData<Unit>()
+    val showAgentsDashboard = MutableLiveData<Unit>()
+
+    fun selectBottomNavItem(menuItem: MenuItem) : Boolean {
+        when (menuItem.itemId) {
+            R.id.menu_home -> {
+                showHomeDashboard.postValue(Unit)
+                return true
+            }
+            R.id.menu_assets -> {
+                showAssetsDashboard.postValue(Unit)
+                return true
+            }
+            R.id.menu_help_center -> {
+                showHelpCenterDashboard.postValue(Unit)
+                return true
+            }
+            R.id.menu_agents -> {
+                showAgentsDashboard.postValue(Unit)
+                return true
+            }
+            else -> return false
+        }
+    }
 }
