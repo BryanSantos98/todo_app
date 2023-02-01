47b98f3af8b0e4b83f43d8ca13d87b81eada7d85
L-Mosca <88050952+L-Mosca@users.noreply.github.com>
2023-02-01 19:49:57 -0300

    Adjust bottom navigation menu item refresh status and add new screen style based in ViewPager position.

--- app/src/main/java/com/example/todo_app/screen/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/todo_app/screen/dashboard/DashboardFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.viewpager2.widget.ViewPager2
+import com.example.todo_app.R
 import com.example.todo_app.databinding.FragmentDashboardBinding
 import com.example.todo_app.screen.dashboard.adapter.ViewPagerAdapter
 import com.example.todo_app.screen.dashboardagents.AgentsDashboardFragment
@@ -16,8 +19,25 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class DashboardFragment : Fragment() {
+
+    companion object {
+        const val HOME_INDEX = 0
+        const val ASSETS_INDEX = 1
+        const val HELP_CENTER_INDEX = 2
+        const val AGENTS_INDEX = 3
+    }
+
+    enum class Type {
+        HOME,
+        ASSETS,
+        HELP_CENTER,
+        AGENTS
+    }
+
     private var _binding: FragmentDashboardBinding? = null
     private val binding get() = _binding!!
+
+    private var actualScreen = Type.HOME
 
     val viewModel: DashboardViewModel by viewModels()
     lateinit var adapter: ViewPagerAdapter
@@ -38,33 +58,56 @@ class DashboardFragment : Fragment() {
         initObservers()
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
     private fun initViews() {
         setViewPager()
         setBottomNavigationListener()
     }
 
     private fun initObservers() {
-        viewModel.showHomeDashboard.observe(viewLifecycleOwner) {
-            if (binding.vpDashboardScreen.currentItem < adapter.itemCount - 1)
-                binding.vpDashboardScreen.currentItem += 1
+        viewModel.setHomeStyle.observe(viewLifecycleOwner) {
+            actualScreen = Type.HOME
+            setScreenDataAndStyle(
+                screenTitle = "Inventory Management",
+                topImageBackground = R.drawable.dashboard_home_gradient
+            )
         }
-        viewModel.showAssetsDashboard.observe(viewLifecycleOwner) {
-            if (binding.vpDashboardScreen.currentItem < adapter.itemCount - 1)
-                binding.vpDashboardScreen.currentItem += 1
-        }
-        viewModel.showHelpCenterDashboard.observe(viewLifecycleOwner) {
-            if (binding.vpDashboardScreen.currentItem < adapter.itemCount - 1)
-                binding.vpDashboardScreen.currentItem += 1
-        }
-        viewModel.showAgentsDashboard.observe(viewLifecycleOwner) {
-            if (binding.vpDashboardScreen.currentItem < adapter.itemCount - 1)
-                binding.vpDashboardScreen.currentItem += 1
-        }
-    }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        viewModel.setAssetsStyle.observe(viewLifecycleOwner) {
+            actualScreen = Type.ASSETS
+            setScreenDataAndStyle(
+                screenTitle = "Assets",
+                topImageBackground = R.drawable.dashboard_assets_gradient
+            )
+        }
+
+        viewModel.setHelpCenterStyle.observe(viewLifecycleOwner) {
+            actualScreen = Type.HELP_CENTER
+            setScreenDataAndStyle(
+                screenTitle = "Help Center",
+                topImageBackground = R.drawable.dashboard_home_gradient
+            )
+        }
+
+        viewModel.setAgentsStyle.observe(viewLifecycleOwner) {
+            actualScreen = Type.AGENTS
+            setScreenDataAndStyle(
+                screenTitle = "Agents",
+                topImageBackground = R.drawable.dashboard_home_gradient
+            )
+        }
+
+        viewModel.setMenuIcon.observe(viewLifecycleOwner) {
+            binding.bnvDashboard.menu.getItem(it.first).isChecked = it.second
+        }
+
+        viewModel.switchFragment.observe(viewLifecycleOwner) { fragmentIndex ->
+            binding.vpDashboardScreen.setCurrentItem(fragmentIndex, true)
+        }
     }
 
     private fun setViewPager() {
@@ -83,5 +126,21 @@ class DashboardFragment : Fragment() {
         binding.bnvDashboard.setOnItemSelectedListener { menuItem ->
             viewModel.selectBottomNavItem(menuItem)
         }
+
+        binding.vpDashboardScreen.registerOnPageChangeCallback(object :
+            ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                // Refresh bottomNavigationView menu icon state when viewPager switch fragment with scroll
+                viewModel.setBottomMenuIcon(binding.bnvDashboard.menu.getItem(position).itemId)
+                viewModel.setScreenDataAndStyle(position)
+                super.onPageSelected(position)
+            }
+        })
+    }
+
+    private fun setScreenDataAndStyle(screenTitle: String, topImageBackground: Int) {
+        binding.tvTitle.text = screenTitle
+        binding.ivBackground.background =
+            ContextCompat.getDrawable(requireContext(), topImageBackground)
     }
 }
--- app/src/main/java/com/example/todo_app/screen/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/example/todo_app/screen/dashboard/DashboardViewModel.kt
@@ -1,7 +1,7 @@
 package com.example.todo_app.screen.dashboard
 
+import android.util.Log
 import android.view.MenuItem
-import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.todo_app.R
@@ -11,30 +11,56 @@ import javax.inject.Inject
 @HiltViewModel
 class DashboardViewModel @Inject constructor() : ViewModel() {
 
-    val showHomeDashboard = MutableLiveData<Unit>()
-    val showAssetsDashboard = MutableLiveData<Unit>()
-    val showHelpCenterDashboard = MutableLiveData<Unit>()
-    val showAgentsDashboard = MutableLiveData<Unit>()
+    val switchFragment = MutableLiveData<Int>()
+    val setMenuIcon = MutableLiveData<Pair<Int, Boolean>>()
+    val setHomeStyle = MutableLiveData<Unit>()
+    val setAssetsStyle = MutableLiveData<Unit>()
+    val setHelpCenterStyle = MutableLiveData<Unit>()
+    val setAgentsStyle = MutableLiveData<Unit>()
 
-    fun selectBottomNavItem(menuItem: MenuItem) : Boolean {
+    fun selectBottomNavItem(menuItem: MenuItem): Boolean {
         when (menuItem.itemId) {
             R.id.menu_home -> {
-                showHomeDashboard.postValue(Unit)
+                switchFragment.postValue(DashboardFragment.HOME_INDEX)
                 return true
             }
             R.id.menu_assets -> {
-                showAssetsDashboard.postValue(Unit)
+                switchFragment.postValue(DashboardFragment.ASSETS_INDEX)
                 return true
             }
             R.id.menu_help_center -> {
-                showHelpCenterDashboard.postValue(Unit)
+                switchFragment.postValue(DashboardFragment.HELP_CENTER_INDEX)
                 return true
             }
             R.id.menu_agents -> {
-                showAgentsDashboard.postValue(Unit)
+                switchFragment.postValue(DashboardFragment.AGENTS_INDEX)
                 return true
             }
             else -> return false
         }
+    }
+
+    fun setBottomMenuIcon(menuItemId: Int) {
+        when (menuItemId) {
+            R.id.menu_home -> setMenuIcon.postValue(Pair(DashboardFragment.HOME_INDEX, true))
+            R.id.menu_assets -> setMenuIcon.postValue(Pair(DashboardFragment.ASSETS_INDEX, true))
+            R.id.menu_help_center -> setMenuIcon.postValue(
+                Pair(
+                    DashboardFragment.HELP_CENTER_INDEX,
+                    true
+                )
+            )
+            R.id.menu_agents -> setMenuIcon.postValue(Pair(DashboardFragment.AGENTS_INDEX, true))
+        }
+    }
+
+    fun setScreenDataAndStyle(pageIndex: Int) {
+        when (pageIndex) {
+            DashboardFragment.HOME_INDEX -> setHomeStyle.postValue(Unit)
+            DashboardFragment.ASSETS_INDEX -> setAssetsStyle.postValue(Unit)
+            DashboardFragment.HELP_CENTER_INDEX -> setHelpCenterStyle.postValue(Unit)
+            DashboardFragment.AGENTS_INDEX -> setAgentsStyle.postValue(Unit)
+        }
+
     }
 }
